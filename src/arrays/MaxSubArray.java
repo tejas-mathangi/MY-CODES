13fbd7b422b128ce25dbe7b0192b6f6464e714bf
Tejas Mathangi <mathangi.tejas@gmail.com>
2025-06-01 16:55:49 +0530

    Maximum Subarray-Leetcode

---
+++ src/arrays/MaxSubArray.java
@@ -0,0 +1,12 @@
+public class MaxSubArray {
+    public static void main(String[] args) {
+
+    }
+    public int maxSubArray(int[] nums) {
+        int max_sum = nums[0];
+        int present_sum = 0;
+
+        
+        return 0;
+    }
+}
